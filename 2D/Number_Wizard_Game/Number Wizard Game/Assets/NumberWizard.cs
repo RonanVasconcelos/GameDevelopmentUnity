720c1600af393163c8be93e5ff601c3a17596f0b
Ronan Vasconcelos <ronan_int@hotmail.com>
2019-11-18 16:34:32 -0800

    "nov 18 update"

---
+++ 2D/Number_Wizard_Game/Number Wizard Game/Assets/NumberWizard.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int max = 1000;
+        int min = 1;
+        Debug.Log("Welcome to Number Wizard!");
+        Debug.Log("Pick a Number and do not tell me what it is...");
+        Debug.Log("The Highest number you can choose is " +max);
+        Debug.Log("The Lowest number you can choose is " +min);
+        Debug.Log("Tell me if your number is higher or lower than 500");
+        Debug.Log("Push Up = Higher, Push Down = Lower, Push Enter = Correct");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
