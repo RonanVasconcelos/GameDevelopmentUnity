83eada03e7b0dbe77f71ff63ca09125e81bc738d
Ronan Vasconcelos <ronan_int@hotmail.com>
2019-11-19 17:32:41 -0800

    feature to recognize the keys

--- 2D/Number_Wizard_Game/Number Wizard Game/Assets/NumberWizard.cs
+++ 2D/Number_Wizard_Game/Number Wizard Game/Assets/NumberWizard.cs
@@ -14,12 +14,28 @@ public class NumberWizard : MonoBehaviour
         Debug.Log("The Highest number you can choose is " +max);
         Debug.Log("The Lowest number you can choose is " +min);
         Debug.Log("Tell me if your number is higher or lower than 500");
-        Debug.Log("Push Up = Higher, Push Down = Lower, Push Enter = Correct");
+        Debug.Log("Push Up = Higher, Push Down = Lower, Push Enter = Correct"); 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Debug.Log("Up Arrow key was pressed!");
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Debug.Log("Down Arrow key was pressed!");
+        }
+        else if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Debug.Log("Enter Key was pressed!");
+        }
+        else
+        {
+            Debug.Log("You Should Press \"Up Arrow\" if the number is bigger, \"Down Arrow\" if is smaller or \"Enter\" if is the right answer");
+            Update();
+        }
     }
 }
